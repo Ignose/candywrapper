9fc7b4dc3b3d16bb9cd339990860bf1b92ee0e35
Ignose <ihpares@gmail.com>
2023-11-08 16:27:00 -0500

    Relay fix?

--- src/relay.ts
+++ src/relay.ts
@@ -64,8 +64,6 @@ export function main() {
   if (handleApiRequest()) return;
 
   write(
-    generateHTML(convertArgsToHtml(), {
-      css: ".meat,.meat div{position:absolute}.meat{width:100%;height:100%;top:0;left:0;overflow:hidden;opacity: 10%;z-index: 10;pointer-events: none}.meat div{display:block}.meat div:first-child{left:20%;animation:15s linear -2s infinite fall}.meat div:nth-child(2){left:70%;animation:15s linear -4s infinite fall}.meat div:nth-child(3){left:10%;animation:20s linear -7s infinite fall}.meat div:nth-child(4){left:50%;animation:18s linear -5s infinite fall}.meat div:nth-child(5){left:85%;animation:14s linear -5s infinite fall}.meat div:nth-child(6){left:15%;animation:16s linear -10s infinite fall}.meat div:nth-child(7){left:90%;animation:15s linear -4s infinite fall}@keyframes fall{0%{opacity:1;top:-10%;transform:translateX (20px) rotate(0)}20%{opacity:.8;transform:translateX (-20px) rotate(45deg)}40%{transform:translateX (-20px) rotate(90deg)}60%{transform:translateX (-20px) rotate(135deg)}80%{transform:translateX (-20px) rotate(180deg)}100%{top:110%;transform:translateX (-20px) rotate(225deg)}}",
-    })
+    generateHTML(convertArgsToHtml())
   );
 }
