2b5e628c175e41e4d1cabab7494070ac062c537c
Ignose <ihpares@gmail.com>
2025-01-22 06:03:34 -0500

    Cyberrealm tweaks

--- src/tasks/repeatableTasks.ts
+++ src/tasks/repeatableTasks.ts
@@ -253,7 +253,7 @@ export function postRunQuests(): Task[] {
     },
     {
       name: "Run CyberRealm",
-      ready: () => mallPrice($item`1`) > 1_000 && myAdventures() > 60,
+      ready: () => mallPrice($item`1`) > 2_000 && myAdventures() > 60 && myInebriety() < inebrietyLimit(),
       prepare: () => {
         $effects`Astral Shell, Elemental Saucesphere, Scarysauce`.forEach((ef) => {
           if (!have(ef)) useSkill(toSkill(ef));
